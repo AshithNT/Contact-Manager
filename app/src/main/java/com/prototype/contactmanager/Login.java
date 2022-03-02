6e9898eea1b116d9b7c6f35344081fbac9a23715
Ashi <ashithdashi@gmail.com>
2022-03-03 02:08:41 +0530

    phase 2

--- app/src/main/java/com/prototype/contactmanager/Login.java
+++ app/src/main/java/com/prototype/contactmanager/Login.java
@@ -1,17 +1,96 @@
 package com.prototype.contactmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+EditText editText_Email,editText_Password;
+TextView textView_reg,textView_forgot;
+Button button_login;
+FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        startActivity(new Intent(getApplicationContext(),Register.class));
+        //startActivity(new Intent(getApplicationContext(),Register.class));
+
+        EditText email=(EditText)findViewById(R.id.editText_Email);
+        EditText password=(EditText)findViewById(R.id.editText_Password);
+        TextView reg=(TextView)findViewById(R.id.textView_reg);
+        TextView forgot=(TextView)findViewById(R.id.textView_forgot);
+        Button login=(Button)findViewById(R.id.button_login);
+
+        fAuth=FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=email.getText().toString().trim();
+                String pass=password.getText().toString().trim();
+                
+
+                if(TextUtils.isEmpty(mail)){
+                    email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Logged in successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Login.this,"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+        });
+
+
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/prototype/contactmanager/MainActivity.java
+++ app/src/main/java/com/prototype/contactmanager/MainActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button_logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_logout=(Button)findViewById(R.id.button_logout);
 
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
 
     }
 }
